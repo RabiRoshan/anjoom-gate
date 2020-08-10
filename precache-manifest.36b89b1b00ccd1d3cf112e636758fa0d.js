6be68f47954836e09d2c327f2203f1450277be44
Rabi Roshan <rabiroshankottampara@gmail.com>
2020-08-10 16:23:23 +0530

    Updates

---
+++ precache-manifest.36b89b1b00ccd1d3cf112e636758fa0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af31b840530dd48b61b09246f6ff4f9",
+    "url": "/anjoom-gate/index.html"
+  },
+  {
+    "revision": "d4b6e068aecb043f1a10",
+    "url": "/anjoom-gate/static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "32f9a148a98b0d1cab19",
+    "url": "/anjoom-gate/static/css/main.f835bafb.chunk.css"
+  },
+  {
+    "revision": "d4b6e068aecb043f1a10",
+    "url": "/anjoom-gate/static/js/2.64d96d16.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/anjoom-gate/static/js/2.64d96d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f9a148a98b0d1cab19",
+    "url": "/anjoom-gate/static/js/main.5f9cbc0b.chunk.js"
+  },
+  {
+    "revision": "1f1c715759cfd42efaaf",
+    "url": "/anjoom-gate/static/js/runtime-main.7aae1a56.js"
+  }
+]);
